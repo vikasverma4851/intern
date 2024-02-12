c8b3a96d72efed9437d007684afc0e42df5f4f8c
Vikas Verma <vikasverma4851@gmail.com>
2024-02-12 17:06:42 +0530

    Adding all files

---
+++ src/components/MainPage.jsx
@@ -0,0 +1,14 @@
+import React from 'react'
+import './MainPage.css';
+const MainPage = () => {
+  return (
+    <div className='MainPage' >
+      <nav>
+      <h1>Hello</h1>
+      </nav>
+      
+    </div>
+  )
+}
+
+export default MainPage
