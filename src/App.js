cbbf6f32c8e2a647a1adc26d7c8eb89e0152c4e0
Vikas Verma <vikasverma4851@gmail.com>
2024-02-13 14:36:08 +0530

    changing app.jsx

--- src/App.js
+++ src/App.js
@@ -1,30 +1,18 @@
-import react from 'react'
-import LoginForm from './components/LoginForm';
-import MainPage from './components/MainPage';
-import { BrowserRouter,Routes,Route } from 'react-router-dom';
+
+import LoginForm from "./components/LoginForm";
+import MainPage from "./components/MainPage";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
 function App() {
-
-    
-
   return (
-    <div >
-    <BrowserRouter>
-      <Routes>
-        <Route path='/' element={ <LoginForm /> } />
-        <Route path='/mainPage' element={ <MainPage /> } />
-        <Route path='/About' element={ <mainPage /> } />
-       
-        </Routes>  
-
-
-
-    
-     
-    
-
-    </BrowserRouter>
+    <div>
+      <BrowserRouter>
+        <Routes>
+          <Route path="/" element={<LoginForm />} />
+          <Route path="/mainPage" element={<MainPage />} />
+          <Route path="/About" element={<mainPage />} />
+        </Routes>
+      </BrowserRouter>
     </div>
-    
   );
 }
 
